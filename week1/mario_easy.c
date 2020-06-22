c21a85aa33efae8f208a33f3e7be05082e91b5b2
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-22 16:43:20 -0400

    mario finished

---
+++ week1/mario_easy.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+
+void hash(int s);
+void space(int s);
+
+
+int main(void){
+    int height = get_int("Enter the  height: ");
+    int n1 = 0;
+    int n2 = height;
+    for(int i=0;i<=height;i++){
+        space(n2);
+        hash(n1);
+        n1++;
+        n2--;
+    }
+}
+
+void space(int s)
+{
+    for(int i = 0;i < s;i++){
+        printf(" ");
+    }
+}
+
+void hash(int s)
+{
+    for(int i = 0;i < s;i++){
+        printf("#");
+    }
+}
